5b0554338a1674baf58d6dec267e22715cfbc3d3
mipt-alexa <volkova.aa@phystech.edu>
2023-01-17 00:09:16 +0300

    add test sum of errors using unfolding matrices

--- test_truncation.py
+++ test_truncation.py
@@ -1,6 +1,7 @@
 from helper_functions import *
 import unittest
 import numpy as np
+import copy
 
 MPS_LEN = 6  # number of components of MPS
 MAX_DIM = 6  # maximum dimension of any mps component index
@@ -9,7 +10,7 @@ MAX_DIM = 6  # maximum dimension of any mps component index
 class TestTrunc(unittest.TestCase):
     def test_norm_alteration(self):
         mps = create_mps(MPS_LEN, MAX_DIM)
-        init_mps = mps
+        init_mps = copy.deepcopy(mps)
 
         init_norm = get_norm_from_MPS(mps)
 
@@ -21,7 +22,34 @@ class TestTrunc(unittest.TestCase):
         print(init_norm, trunc_val, final_norm)
 
         self.assertLessEqual(final_norm, init_norm)
-        self.assertLessEqual(mps.dot(mps - init_mps) - init_mps.dot(mps - init_mps), trunc_val**2)
+
+        init_tensor = get_tensor_from_MPS(init_mps)
+        final_tensor = get_tensor_from_MPS(mps)
+        diff = init_tensor - final_tensor
+
+        self.assertLessEqual(jnp.tensordot(final_tensor, diff, mps.len + 2) - \
+                             jnp.tensordot(init_tensor, diff, mps.len + 2), trunc_val ** 2)
+
+    def test_sum_errors(self):
+        mps = create_mps(MPS_LEN, MAX_DIM)
+        init_norm = get_norm_from_MPS(mps)
+        init_mps = copy.deepcopy(mps)
+
+        mps.left_canonical()
+        trunc_val = np.random.uniform(0, init_norm / 2)
+        mps.left_svd_trunc(trunc_val)
+
+        total_error_sq = 0
+        for i in range(mps.len - 1):
+            matrix_full = unfold_matrix_from_mps(init_mps, i)
+            matrix_trunc = unfold_matrix_from_mps(mps, i)
+
+            total_error_sq += la.norm(matrix_full - matrix_trunc) ** 2
+
+        diff = get_tensor_from_MPS(init_mps) - get_tensor_from_MPS(mps)
+        trunc_error = np.sqrt(jnp.tensordot(diff, diff, mps.len + 2))
+
+        self.assertLessEqual(trunc_error, np.sqrt(total_error_sq))
 
 
 unittest.main(argv=[''], verbosity=1, exit=True)
