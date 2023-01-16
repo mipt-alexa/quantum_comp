9945f780aa6f3fafdd4f8fde245004243775f33d
mipt-alexa <volkova.aa@phystech.edu>
2023-01-16 18:39:37 +0300

    condition edited in test_norm_alteration

--- test_truncation.py
+++ test_truncation.py
@@ -9,6 +9,7 @@ MAX_DIM = 6  # maximum dimension of any mps component index
 class TestTrunc(unittest.TestCase):
     def test_norm_alteration(self):
         mps = create_mps(MPS_LEN, MAX_DIM)
+        init_mps = mps
 
         init_norm = get_norm_from_MPS(mps)
 
@@ -19,8 +20,8 @@ class TestTrunc(unittest.TestCase):
         final_norm = get_norm_from_MPS(mps)
         print(init_norm, trunc_val, final_norm)
 
-        self.assertLess(final_norm, init_norm)
-        self.assertLess(init_norm - trunc_val, final_norm)
+        self.assertLessEqual(final_norm, init_norm)
+        self.assertLessEqual(mps.dot(mps - init_mps) - init_mps.dot(mps - init_mps), trunc_val**2)
 
 
 unittest.main(argv=[''], verbosity=1, exit=True)
