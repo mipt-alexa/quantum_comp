54d7e0edcbd9bfa63b68683b0716d341d9d8a906
mipt-alexa <volkova.aa@phystech.edu>
2023-02-14 20:50:50 +0300

    Add final version of maxvol with advanced first step

--- maxvol.py
+++ maxvol.py
@@ -1,8 +1,11 @@
 import jax.numpy as jnp
+from jax.scipy.linalg import solve_triangular, lu
 import copy
 
+from typing import Tuple
 
-def arg_absmax(arr: jnp.ndarray) -> tuple:
+
+def arg_absmax(arr: jnp.ndarray) -> Tuple[int, int]:
     """
     This function returns the absolute maximum among 2d-array entries
     """
@@ -13,6 +16,58 @@ def arg_absmax(arr: jnp.ndarray) -> tuple:
     else:
         i, j = amin // arr.shape[1], amin % arr.shape[1]
     return i, j
+
+
+def maxvol(matrix: jnp.ndarray, tol: float = 1e-3) -> jnp.ndarray:
+    """
+    This function implements the maxvol algorithm by using PLU decomposition
+    on the first step and Sherman-Woodbury-Morrison formula
+    for matrix inverse on the following steps
+
+    Args:
+        matrix: 2-dimensional array
+        tol: tolerance, the stopping criterion in iterations
+
+    Returns:
+        The maximum volume submatrix.
+    """
+
+    A = copy.deepcopy(matrix)
+
+    if A.shape[0] < A.shape[1]:
+        raise Exception("Condition on matrix dimensions is not satisfied")
+
+    n, r = A.shape
+    steps = 0
+
+    P, L, U = lu(A)
+
+    Q = solve_triangular(jnp.transpose(U), jnp.transpose(A), lower=True)
+    B = jnp.transpose(solve_triangular(jnp.transpose(L[0:r]), Q, lower=False))
+
+    i, j = arg_absmax(B)
+
+    A = jnp.tensordot(jnp.transpose(P), A, 1)
+
+    while not abs(B[i][j]) < 1. + tol:
+        buff = A[i, :]
+        A = A.at[i, :].set(A[j, :])
+        A = A.at[j, :].set(buff)
+
+        e_i, e_j, e_j_T = jnp.zeros(n), jnp.zeros(n), jnp.zeros(r)
+        e_i = e_i.at[i].set(1)
+        e_j = e_j.at[j].set(1)
+        e_j_T = e_j_T.at[j].set(1)
+
+        B -= 1 / B[i][j] * jnp.tensordot(B[:, j], B[i, :] - e_j_T, 0)
+
+        i, j = arg_absmax(B)
+
+        steps += 1
+        if steps == jnp.power(n, r + 1):
+            raise Exception("computation limit exceeded")
+
+    return A[0:r]
 
 
 def naive_maxvol(matrix: jnp.ndarray, tol: float = 1e-3) -> jnp.ndarray:
@@ -42,7 +97,9 @@ def naive_maxvol(matrix: jnp.ndarray, tol: float = 1e-3) -> jnp.ndarray:
 
     while not abs(B[i][j]) < 1. + tol:
 
-        A[[j, i], ] = A[[i, j], ]
+        buff = A[i, :]
+        A = A.at[i, :].set(A[j, :])
+        A = A.at[j, :].set(buff)
 
         submatrix = A[0:r]
         B = jnp.tensordot(A, jnp.linalg.inv(submatrix), 1)
@@ -71,19 +128,20 @@ def maxvol_SWM(matrix: jnp.ndarray, tol: float = 1e-3) -> jnp.ndarray:
     A = copy.deepcopy(matrix)
 
     if A.shape[0] < A.shape[1]:
-        raise Exception("Condition on Matrix dimensions is not satisfied")
+        raise Exception("Condition on matrix dimensions is not satisfied")
 
     n, r = A.shape
     steps = 0
 
-    submatrix = A[0:r]
-    B = jnp.tensordot(A, jnp.linalg.inv(submatrix), 1)
+    B = jnp.tensordot(A, jnp.linalg.inv(A[0:r]), 1)
+
     i, j = arg_absmax(B)
 
     while not abs(B[i][j]) < 1. + tol:
-        A[[j, i], ] = A[[i, j], ]
 
-        submatrix = A[0:r]
+        buff = A[i, :]
+        A = A.at[i, :].set(A[j, :])
+        A = A.at[j, :].set(buff)
 
         e_i, e_j, e_j_T = jnp.zeros(n), jnp.zeros(n), jnp.zeros(r)
         e_i = e_i.at[i].set(1)
@@ -96,6 +154,6 @@ def maxvol_SWM(matrix: jnp.ndarray, tol: float = 1e-3) -> jnp.ndarray:
 
         steps += 1
         if steps == jnp.power(n, r + 1):
-            raise Exception("computation limit exceed")
+            raise Exception("computation limit exceeded")
 
-    return submatrix
+    return A[0:r]
