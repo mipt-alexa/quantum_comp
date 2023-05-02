1095957f4e1b598e62c5450a33b6c30447b24383
mipt-alexa <volkova.aa@phystech.edu>
2023-05-02 19:29:20 +0300

    Added hilbert tensor decompose

--- hilbert_tensor_decompose.py
+++ hilbert_tensor_decompose.py
@@ -11,33 +11,44 @@ jax.config.update("jax_enable_x64", True)
 
 def hilbert_tensor(ind_arr):
     if len(ind_arr.shape) == 1:
-        return 1 / (jnp.sum(ind_arr) + len(ind_arr))
+        return jnp.array(1 / (jnp.sum(ind_arr) + len(ind_arr)), dtype=jnp.float64)
 
-    return 1 / (len(ind_arr[0]) + jnp.sum(ind_arr, axis=1))
+    return jnp.array(1 / (len(ind_arr[0]) + jnp.sum(ind_arr, axis=1)), dtype=jnp.float64)
 
 
-mps_len = 40
-out_dims = [20] * mps_len
+def main ():
+    mps_len = 50
+    out_dims = [30] * mps_len
 
-for in_dim in range(2, out_dims[0]//2):
-    in_dims = [in_dim]*(mps_len - 1)
-    print(in_dims)
-    t_0 = timeit.default_timer()
-    mps = TT_cross(hilbert_tensor, out_dims, in_dims)
+    len_ = 12
+    deltas = np.empty(len_ - 2)
+    update_time = np.empty(len_ - 2)
+    linalg_time = np.empty(len_ - 2)
 
-    print("in_dim =", in_dim, "time =", timeit.default_timer() - t_0)
+    for in_dim in range(2, len_):
+        in_dims = [in_dim]*(mps_len - 1)
 
-    delta = 0
-    for j in range(100):
+        t_0 = timeit.default_timer()
+        mps, update_time[in_dim - 2], linalg_time[in_dim - 2] = TT_cross(hilbert_tensor, out_dims, in_dims, tol=1e-2, max_iter=6)
 
-        rand_multiind = jnp.array(np.random.choice(range(out_dims[0]), mps_len, replace=True))
+        print("in_dim =", in_dim, "time =", timeit.default_timer() - t_0, "norm=", mps.norm())
 
-        element = mps.components[0][:, rand_multiind[0], :]
-        for i in range(1, mps_len):
-            element = jnp.tensordot(element, mps.components[i][:, rand_multiind[i], :], 1)
+        print(mps.components[0].dtype)
 
-        # print(float(element), float(hilbert_tensor([rand_multiind])))
+        delta = 0
 
-        delta += abs(float(element) - float(hilbert_tensor(rand_multiind)))
+        for j in range(100):
+            rand_multiind = jnp.array(np.random.choice(range(out_dims[0]), mps_len, replace=True))
 
-    print("delta", delta)
+            element = mps.components[0][:, rand_multiind[0], :]
+            for i in range(1, mps_len):
+                element = jnp.tensordot(element, mps.components[i][:, rand_multiind[i], :], 1)
+
+            delta += abs(jnp.float64(element) - jnp.float64(hilbert_tensor(rand_multiind))) / jnp.float64(hilbert_tensor(rand_multiind))
+        deltas[in_dim - 2] = delta / 100
+
+    print(np.column_stack((deltas, update_time, linalg_time)))
+
+
+if __name__ == "__main__":
+    main()
